c334533b75253118205ee7b3c07847e6b617f3ed
DonKihot <donkihot1984@gmail.com>
2024-10-26 21:24:28 +0300

    migration to spring boot started

---
+++ src/main/java/edu/itstep/java_exam/JavaExamApplication.java
@@ -0,0 +1,13 @@
+package edu.itstep.java_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/itstep/java_exam/JavaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itstep.java_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
